a06cfd5c4cfe5305756e6b70adb41cb72202b052
hdm <hdm58@qq.com>
2022-06-27 07:52:24 +0800

    更新layui到2.7.1版本

--- public/index.php
+++ public/index.php
@@ -15,10 +15,10 @@ if (empty(file_exists(__DIR__ . '/../vendor/autoload.php'))) {
 require __DIR__ . '/../vendor/autoload.php';
 
 // 定义当前版本号
-define('CMS_VERSION','3.0.68');
+define('CMS_VERSION','3.6.27');
 
 // 定义Layui版本号
-define('LAYUI_VERSION','2.7.0');
+define('LAYUI_VERSION','2.7.1');
 
 // 定义项目目录
 define('CMS_ROOT', __DIR__ . '/../');
