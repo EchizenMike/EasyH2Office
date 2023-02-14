c5225d7aa850c5d9b7c33a403ca197468d1a70ae
hdm <hdm58@qq.com>
2023-02-14 14:32:55 +0800

    系统支持升级到TinkPHP6.1.2

---
+++ public/static/assets/gougu/module/excel.js
@@ -0,0 +1,33 @@
+layui.define([], function (exports) {
+	var MOD_NAME = 'excel';
+	var modFile = layui.cache.modules['excel'];
+	var modPath = modFile.substr(0, modFile.lastIndexOf('.'));
+	var settings = {
+		name: '表格'
+	};
+	var excel = {
+		init: function (tableId, options) {
+			loadScript();
+			var opts = $.extend({}, settings, options);
+			$('#'+tableId).table2excel({
+				name: opts.name,
+				filename: opts.name + new Date().getTime() + ".xls",
+				exclude: ".noExl",
+				exclude_img: false,
+				exclude_links: false,
+				exclude_inputs: false
+			});
+		}
+	}
+	function loadScript() {
+		if (typeof table2excel == 'undefined') {
+			$.ajax({ //获取插件
+				url: modPath + '/table2excel.js' ,
+				dataType: 'script',
+				cache: true,
+				async: false,
+			});
+		}
+	}
+	exports(MOD_NAME, excel);
+});
