257cd58224f9a8b84398cab0eb5f13b252c15058
hdm <hdm58@qq.com>
2023-02-27 09:36:33 +0800

    代码失误，少写了个$

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -83,7 +83,7 @@ class Index extends BaseController
          $data['action'] = app('request')->action();
          $data['uploadip'] = app('request')->ip();
          $data['create_time'] = time();
-         $data['user_id'] = this->uid;
+         $data['user_id'] = $this->uid;
          if ($data['module'] = 'admin') {
              //通过后台上传的文件直接审核通过
              $data['status'] = 1;
