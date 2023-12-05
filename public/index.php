65575f970baa6635251c4510553c7e79fbd6d926
HDM58\hdm58 <hdm58@qq.com>
2023-12-05 16:01:07 +0800

    1、新增跟多的iconfont图标 2、优化附件管理的操作，重命名、选择文件操作更人性化

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ if (empty(file_exists(__DIR__ . '/../vendor/autoload.php'))) {
 require __DIR__ . '/../vendor/autoload.php';
 
 // 定义当前版本号
-define('CMS_VERSION','4.93.11');
+define('CMS_VERSION','4.95.12');
 
 // 定义Layui版本号
 define('LAYUI_VERSION','2.9.0');
