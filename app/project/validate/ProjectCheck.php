0a21a1b7388d10c8ba48e282f49c145393db2439
HDM58\hdm58 <hdm58@qq.com>
2024-01-15 10:11:52 +0800

    修复： 1、删除项目，列表数据不自动更新问题 2、新增项目提示项目名称已经存在问题修复

--- app/project/validate/ProjectCheck.php
+++ app/project/validate/ProjectCheck.php
@@ -5,22 +5,17 @@ use think\Validate;
 class ProjectCheck extends Validate
 {
     protected $rule = [
-        'name'       => 'require|unique:project',
-        'code'       => 'alphaNum|length:5,10|unique:project',
+        'name'       => 'require',
         'id'         => 'require'
     ];
 
     protected $message = [
         'name.require'           => '项目名称不能为空',
-        'name.unique'            => '同样的项目名称已经存在',
-        'code.alphaNum'          => '项目代码只能为5至10为字母和数字',
-        'code.length'            => '项目代码只能为5至10为字母和数字',
-        'code.unique'            => '同样的项目代码已经存在',
         'id.require'             => '缺少更新条件',
     ];
 
     protected $scene = [
-        'add'       => ['name','code'],
+        'add'       => ['name'],
         'edit'      => ['id']
     ];
 }
