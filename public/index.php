70a87c2b280f000516f25700fdadc49fda5fca10
HDM58\hdm58 <hdm58@qq.com>
2024-03-08 10:44:46 +0800

    更新版本号

--- public/index.php
+++ public/index.php
@@ -15,10 +15,10 @@ if (empty(file_exists(__DIR__ . '/../vendor/autoload.php'))) {
 require __DIR__ . '/../vendor/autoload.php';
 
 // 定义当前版本号
-define('CMS_VERSION','4.96.16');
+define('CMS_VERSION','4.97.38');
 
 // 定义Layui版本号
-define('LAYUI_VERSION','2.9.3');
+define('LAYUI_VERSION','2.9.7');
 
 // 定义项目目录
 define('CMS_ROOT', __DIR__ . '/../');
