d59f6ffb0e67f8a4961d14d5d7788369a83ed712
HDM58\hdm58 <hdm58@qq.com>
2024-05-08 00:29:58 +0800

    更新layui到最新的2.9.9版本

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ require __DIR__ . '/../vendor/autoload.php';
 define('CMS_VERSION','4.98.12');
 
 // 定义Layui版本号
-define('LAYUI_VERSION','2.9.8');
+define('LAYUI_VERSION','2.9.9');
 
 // 定义项目目录
 define('CMS_ROOT', __DIR__ . '/../');
