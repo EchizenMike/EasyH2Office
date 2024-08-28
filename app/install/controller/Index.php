3d84bdcc20456b19bf82739299166892adadc2d6
HDM58\hdm58 <hdm123@qq.com>
2024-08-28 18:34:02 +0800

    勾股5.0安装bug修复

--- app/install/controller/Index.php
+++ app/install/controller/Index.php
@@ -158,6 +158,7 @@ class Index
             return to_assign(1, '创建管理员信息失败');
         }
         $link->close();
+        $domain = get_system_config('web','domain');
         $db_str = "<?php
 return [
     // 默认使用的数据库连接配置
@@ -220,7 +221,6 @@ return [
         if (false == file_put_contents(CMS_ROOT . "config/install.lock", '勾股OA安装鉴定文件，请勿删除！！！！！此次安装时间为：' . date('Y-m-d H:i:s', time()))) {
             return to_assign(1, '创建安装鉴定文件失败，请检查目录权限');
         }
-        $domain = get_system_config('web','domain');
         return to_assign(0,'安装完成',$domain);
     }
 }
