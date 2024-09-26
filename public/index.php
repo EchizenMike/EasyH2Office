74d88fd4c79b415c25c663d7d1957f52a51b6fc8
HDM58\hdm58 <hdm123@qq.com>
2024-09-26 10:02:17 +0800

    layui升级到最新的2.9.17稳定版

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ require __DIR__ . '/../vendor/autoload.php';
 define('CMS_VERSION','5.1.2');
 
 // 定义Layui版本号
-define('LAYUI_VERSION','2.9.16');
+define('LAYUI_VERSION','2.9.17');
 
 // 定义项目目录
 define('CMS_ROOT', __DIR__ . '/../');
